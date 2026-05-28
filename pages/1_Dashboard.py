ea3cd30f69473ecc6b201ce7b599cec146159232
jhersonperez <jhersonperez3@gmail.com>
2026-05-28 15:25:48 -0500

    Ajustar la visual del tablero power bi

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -3,6 +3,8 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Dashboard RCV · Power BI", layout="wide")
 
+#Componete de power bi
+
 components.iframe(
     src="https://app.powerbi.com/view?r=eyJrIjoiMWM3ZTk4ZTYtYzY4ZC00MDYxLWI2NjYtNGE4NjhjZTViZjAzIiwidCI6ImUzOGI2YjNiLWRkMTQtNDVhZi1hZjBhLWU2N2QxYjk2ODQyYSIsImMiOjR9&navContentPaneEnabled=false",
     height=800,
