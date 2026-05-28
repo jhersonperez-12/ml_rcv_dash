4663f533c081e7c0384b202256f5b2468c39198f
jhersonperez <jhersonperez3@gmail.com>
2026-05-28 15:30:59 -0500

    fff

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Dashboard RCV · Power BI", layout="wide")
+#st.set_page_config(page_title="Dashboard RCV · Power BI", layout="wide")
 
 #Componete de power bi
 
